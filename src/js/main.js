9408baeb4063493d32b6ce73bf218f5dd1ad733a
Herlin Matos <herlin.matos@gmail.com>
2019-11-18 00:25:42 -0500

    add evaluate button

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,15 @@
 const { parseEbnf, createDocumentation } = require("ebnf2railroad");
 
-const ebnf = document.getElementById("ebnf-textarea").value;
-const ast = parseEbnf(ebnf); // can throw parse error exceptions
-const htmlOutput = createDocumentation(ast,{});
+document.getElementById("ebnf-evaluate-btn").addEventListener("click", evaluateEbnfInput);
 
-const container = document.getElementById("container");
-const astContainer = document.getElementById("ast-container");
-
-container.innerHTML = htmlOutput;
-astContainer.innerText = JSON.stringify(ast);
+function evaluateEbnfInput(){
+    const ebnf = document.getElementById("ebnf-textarea").value;
+    const ast = parseEbnf(ebnf); // can throw parse error exceptions
+    const htmlOutput = createDocumentation(ast,{});
+    
+    const container = document.getElementById("container");
+    const astContainer = document.getElementById("ast-container");
+    
+    container.innerHTML = htmlOutput;
+    astContainer.innerText = JSON.stringify(ast);
+}
