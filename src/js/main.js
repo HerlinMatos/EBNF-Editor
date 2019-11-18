e936a935092bcaf82cd22e678d04d8b5c8b4de59
Herlin Matos <herlin.matos@gmail.com>
2019-11-18 00:13:20 -0500

    add textarea to edit ebnf input

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 const { parseEbnf, createDocumentation } = require("ebnf2railroad");
 
-const ebnf = "definition = 'a', other, { other } | item, 'b';";
+const ebnf = document.getElementById("ebnf-textarea").value;
 const ast = parseEbnf(ebnf); // can throw parse error exceptions
 const htmlOutput = createDocumentation(ast,{});
 
