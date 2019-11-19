132dfded19487187445c9c0087d97b86a9e03ea6
Herlin Matos <herlin.matos@gmail.com>
2019-11-19 00:54:24 -0500

    drawing ast first approach

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,25 @@ import Tree from 'react-d3-tree';
 
 document.getElementById("ebnf-evaluate-btn").addEventListener("click", evaluateEbnfInput);
 
+function traverse(node){
+    const isObject = (n) => typeof n === 'object' && n.constructor === Object;
+    const isArray = (n) => typeof n === 'object' && n.constructor === Array;
+    
+    if(isObject(node)){
+        const name = "";
+        // const nodeArray = Object.keys(node).map(k => ({[k] : node[k]}));
+        const nodeArray = Object.values(node);
+        const children = nodeArray.map(traverse);
+        return {name, children};
+    }else if(isArray(node)){
+        const name = "";
+        const children = node.map(traverse);
+        return {name, children};
+    }else{
+        return {name: node.toString()};
+    }
+}
+
 function evaluateEbnfInput(){
     const ebnf = document.getElementById("ebnf-textarea").value;
     const ast = parseEbnf(ebnf); // can throw parse error exceptions
@@ -14,7 +33,10 @@ function evaluateEbnfInput(){
     const astContainer = document.getElementById("ast-container");
     const treeViewContainer = document.getElementById("tree-view-container");
     
+    let newAst = traverse(ast);
+    console.log(newAst);
+    
     container.innerHTML = htmlOutput;
-    astContainer.innerText = JSON.stringify(ast);
-    ReactDOM.render(<Tree data={ast} />, treeViewContainer);
+    astContainer.innerText = JSON.stringify(newAst);
+    ReactDOM.render(<Tree data={newAst} />, treeViewContainer);
 }
