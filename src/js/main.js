78c19e9bbefc70edd31c379e3260d7196ee794d3
Herlin Matos <herlin.matos@gmail.com>
2019-11-21 03:14:54 -0500

    Create tree-utils module

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,10 @@
 const { parseEbnf, createDocumentation } = require("ebnf2railroad");
+const { formatTree } = require("./tree-utils.js");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Tree from 'react-d3-tree';
 
 document.getElementById("ebnf-evaluate-btn").addEventListener("click", evaluateEbnfInput);
-
-function traverse(node){
-    const isObject = (n) => typeof n === 'object' && n.constructor === Object;
-    const isArray = (n) => typeof n === 'object' && n.constructor === Array;
-    
-    if(isObject(node)){
-        const name = "";
-        const attributes = Object.keys(node);
-        // const nodeArray = Object.keys(node).map(k => ({[k] : node[k]}));
-        const nodeArray = Object.values(node);
-        const children = nodeArray.map(traverse);
-        return {name, attributes, children};
-    }else if(isArray(node)){
-        const name = "Array";
-        const children = node.map(traverse);
-        return {name, children};
-    }else{
-        return {name: node.toString()};
-    }
-}
 
 function evaluateEbnfInput(){
     const ebnf = document.getElementById("ebnf-textarea").value;
@@ -34,8 +15,7 @@ function evaluateEbnfInput(){
     const astContainer = document.getElementById("ast-container");
     const treeViewContainer = document.getElementById("tree-view-container");
     
-    let newAst = traverse(ast);
-    console.log(newAst);
+    const newAst = formatTree(ast);
     
     container.innerHTML = htmlOutput;
     astContainer.innerText = JSON.stringify(newAst);
---
+++ src/js/tree-utils.js
@@ -0,0 +1,24 @@
+
+function formatTree(node){
+    const isObject = (n) => typeof n === 'object' && n.constructor === Object;
+    const isArray = (n) => typeof n === 'object' && n.constructor === Array;
+    
+    if(isObject(node)){
+        const name = "";
+        const attributes = Object.keys(node);
+        // const nodeArray = Object.keys(node).map(k => ({[k] : node[k]}));
+        const nodeArray = Object.values(node);
+        const children = nodeArray.map(formatTree);
+        return {name, attributes, children};
+    }else if(isArray(node)){
+        const name = "Array";
+        const children = node.map(formatTree);
+        return {name, children};
+    }else{
+        return {name: node.toString()};
+    }
+}
+
+module.exports = { 
+    formatTree
+};
