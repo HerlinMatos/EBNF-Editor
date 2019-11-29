8c3b23acfc2b3e863cf9ecd0d009593a62f1e283
Herlin Matos <herlin.matos@gmail.com>
2019-11-29 12:30:40 -0500

    fix: manage ast nodes with arrays childs
    Avoid the new creation of node to content the array

--- src/js/tree-utils.js
+++ src/js/tree-utils.js
@@ -1,22 +1,25 @@
+const isObject = (n) => typeof n === 'object' && n.constructor === Object;
+const isArray = (n) => typeof n === 'object' && n.constructor === Array;
 
-function formatTree(node){
-    const isObject = (n) => typeof n === 'object' && n.constructor === Object;
-    const isArray = (n) => typeof n === 'object' && n.constructor === Array;
-
+function formatTree(node, noRoot){
     if(isObject(node)){
         if(node.terminal)
             return {name: node.terminal}
         if(node.nonTerminal)
             return {name: node.nonTerminal}
+        if(Object.values(node).length == 1 && isArray(Object.values(node)[0])){
+            return {name: Object.keys(node)[0], children: Object.values(node).map((n) => formatTree(n,true))[0]}
+        }
         const name = node.identifier || '';
         const attributes = Object.keys(node);
-        // const nodeArray = Object.keys(node).map(k => ({[k] : node[k]}));
         const nodeArray = Object.values(node);
         const children = nodeArray.map(formatTree);
         return {name, attributes, children};
-    }else if(isArray(node)){
-        const name = "Array";
-        const children = node.map(formatTree);
+    }else if(isArray(node) && noRoot){
+        return node.map((n) => formatTree(n,true));
+    }else if(isArray(node) && !noRoot){
+        const name = "Expressions";
+        const children = node.map((n) => formatTree(n,true));
         return {name, children};
     }else{
         return {name: node.toString()};
