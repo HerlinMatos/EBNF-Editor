0711a4915fcd18cf07e2381bbdc387e78582f9e4
Herlin Matos <herlin.matos@gmail.com>
2019-12-01 00:30:57 -0500

    add autocompletion to editor

--- src/js/main.js
+++ src/js/main.js
@@ -46,6 +46,7 @@ ReactDOM.render(<Split
     </div>
     </Split>, document.getElementById('root'));
 
+// ace.require("ace/ext/language_tools");
 var editor = ace.edit("editor");
 
 editor.setTheme("ace/theme/chrome");
