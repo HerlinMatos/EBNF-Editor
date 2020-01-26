ed09114c85ae89995704847012af715fa1579aba
Herlin Matos <herlin.matos@gmail.com>
2020-01-26 14:06:03 -0500

    going inside non terminal nodes

--- src/js/tree-utils.js
+++ src/js/tree-utils.js
@@ -2,34 +2,35 @@ const isObject = (n) => typeof n === 'object' && n.constructor === Object;
 const isArray = (n) => typeof n === 'object' && n.constructor === Array;
 
 //TODO: This function is incomplete and needs some optimizations 
-function formatTree(node, noRoot){
+function formatTree(node, ast, noRoot){
     if(isObject(node)){
         const nodeKeys = Object.keys(node);
         const nodeValues = Object.values(node);
         if(node.terminal)
             return {name: node.terminal}
         if(node.nonTerminal)
-            return {name: node.nonTerminal}
+            return formatTree(ast.find(n => n.identifier == node.nonTerminal), ast, true)
         if(nodeKeys.includes('sequence')){
-            return {name: 'sequence', children: nodeValues.map((n) => formatTree(n,true))[0]}
+            return {name: 'sequence', children: nodeValues.map((n) => formatTree(n,ast,true))[0]}
         }
         if(nodeKeys.includes('repetition')){
-            return {name: 'repetition', attributes: ['skippable: '+node.skippable],children: [formatTree(node.repetition, true)]}
+            return {name: 'repetition', attributes: ['skippable: '+node.skippable],children: [formatTree(node.repetition,ast,true)]}
         }
         if(nodeKeys.includes('choice')){
-            return {name: 'choice', children: nodeValues.map((n) => formatTree(n,true))[0]}
+            return {name: 'choice', children: nodeValues.map((n) => formatTree(n,ast,true))[0]}
         }
         if(nodeKeys.includes('optional')){
-            return {name: 'optional', children: [formatTree(node.optional, true)]}
+            return {name: 'optional', children: [formatTree(node.optional,ast, true)]}
         }
         if(nodeKeys.includes('definition')){
-            return {name: node.identifier, children: [formatTree(node.definition, true)]}
+            return {name: node.identifier, children: [formatTree(node.definition,ast, true)]}
         }
     }else if(isArray(node) && noRoot){
-        return node.filter(n => !n.comment).map((n) => formatTree(n,true));
+        return node.filter(n => !n.comment).map((n) => formatTree(n,ast,true));
     }else if(isArray(node) && !noRoot){
         const name = "Expressions";
-        const children = node.filter(n => !n.comment).map((n) => formatTree(n,true));
+        const ast = node;
+        const children = node.filter(n => !n.comment).map((n) => formatTree(n,ast,true));
         return {name, children};
     }
 }
